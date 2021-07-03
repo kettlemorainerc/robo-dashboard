ad0b4cb5a6408f8f3bb90442e5d4da770f104ffa
David Faris <dfaris@my.wctc.edu>
2021-07-03 15:08:08 -0500

    Only send an update if the NT needs to update it and attempt to make a way to switch between using local NT and bot NT

--- backend/src/main/java/com/squedgy/network/tables/NetworkUtilities.java
+++ backend/src/main/java/com/squedgy/network/tables/NetworkUtilities.java
@@ -2,9 +2,11 @@ package com.squedgy.network.tables;
 
 import edu.wpi.first.networktables.*;
 
+import java.util.*;
 import java.util.function.*;
 
 import static edu.wpi.first.networktables.EntryListenerFlags.*;
+import static edu.wpi.first.networktables.NetworkTableInstance.*;
 
 /**
  * A utility wrapper around Network Tables to utilize various instances
@@ -15,8 +17,21 @@ public final class NetworkUtilities {
     private static final NetworkTableInstance NETWORK_TABLE = NetworkTableInstance.getDefault();
 
     public static void initializeNT() {
-        NETWORK_TABLE.startServer("networktables.ini", "127.0.0.1", NetworkTableInstance.kDefaultPort);
-//        NETWORK_TABLE.startClient("127.0.0.1", NetworkTableInstance.kDefaultPort);
+        setNetworkTableParams(false);
+    }
+
+    public static void setNetworkTableParams(boolean asClient) {
+        setNetworkTableParams(asClient, kDefaultPort);
+    }
+
+    public static void setNetworkTableParams(boolean asClient, int port) {
+        if(asClient) {
+            NETWORK_TABLE.stopServer();
+            NETWORK_TABLE.startClientTeam(2077, port);
+        } else {
+            NETWORK_TABLE.stopClient();
+            NETWORK_TABLE.startServer("networktables.ini", "127.0.0.1", port);
+        }
     }
 
     private NetworkUtilities() {}
@@ -30,23 +45,23 @@ public final class NetworkUtilities {
     }
 
     public static void setBooleanArray(String key, boolean[] val) {
-        getEntry(key).setBooleanArray(val);
+        if(!Arrays.equals(val, getBooleanArray(key))) getEntry(key).setBooleanArray(val);
     }
 
     public static boolean[] getBooleanArray(String key) {
         return getEntry(key).getBooleanArray((boolean[]) null);
     }
 
+    public static void setBoolean(String key, boolean val) {
+        if(val != getBoolean(key))getEntry(key).setBoolean(val);
+    }
+
     public static boolean getBoolean(String key) {
         return getEntry(key).getBoolean(false);
     }
 
-    public static void setBoolean(String key, boolean val) {
-        getEntry(key).setBoolean(val);
-    }
-
     public static void setStringArray(String key, String[] val) {
-        getEntry(key).setStringArray(val);
+        if(!Arrays.equals(val, getStringArray(key))) getEntry(key).setStringArray(val);
     }
 
     public static String[] getStringArray(String key) {
@@ -54,7 +69,7 @@ public final class NetworkUtilities {
     }
 
     public static void setString(String key, String val) {
-        getEntry(key).setString(val);
+        if(!Objects.equals(val, getString(key))) getEntry(key).setString(val);
     }
 
     public static String getString(String key) {
@@ -62,19 +77,19 @@ public final class NetworkUtilities {
     }
 
     public static void setDoubleArray(String key, double[] val) {
-        getEntry(key).setDoubleArray(val);
+        if(!Arrays.equals(val, getDoubleArray(key))) getEntry(key).setDoubleArray(val);
     }
 
     public static double[] getDoubleArray(String key) {
         return getEntry(key).getDoubleArray((double[]) null);
     }
 
-    public static double getDouble(String key) {
-        return getEntry(key).getDouble(Double.NaN);
+    public static void setDouble(String key, double val) {
+        if(val != getDouble(key)) getEntry(key).setDouble(val);
     }
 
-    public static void setDouble(String key, double val) {
-        getEntry(key).setDouble(val);
+    public static double getDouble(String key) {
+        return getEntry(key).getDouble(Double.NaN);
     }
 
     public static void togglePersist(String key) {
@@ -82,7 +97,7 @@ public final class NetworkUtilities {
     }
 
     public static int addListener(Consumer<EntryNotification> listener, int flags) {
-        return NETWORK_TABLE.addEntryListener("", listener, flags); // getNetworkTable(table).addEntryListener(listener, flags);
+        return NETWORK_TABLE.addEntryListener("", listener, flags);
     }
 
     public static int addListener(Consumer<EntryNotification> listener) {
@@ -98,38 +113,20 @@ public final class NetworkUtilities {
         ifExists(key, NetworkTableEntry::delete);
     }
 
+    private static void nothing(NetworkTableEntry entry) {}
 
     private static void ifOrElse(
-        String key,
+        String entryKey,
         Predicate<NetworkTableEntry> only,
         Consumer<NetworkTableEntry> then,
         Consumer<NetworkTableEntry> orElse
     ) {
-        NetworkTableEntry entry = NETWORK_TABLE.getEntry(key);
+        NetworkTableEntry entry = NETWORK_TABLE.getEntry(entryKey);
         if(only.test(entry)) then.accept(entry);
         else orElse.accept(entry);
     }
 
-    private static <T> T ifOrElse(
-        String key,
-        Predicate<NetworkTableEntry> when,
-        T arg,
-        BiFunction<NetworkTableEntry, T, T> then,
-        BiFunction<NetworkTableEntry, T, T> orElse
-    ) {
-        NetworkTableEntry entry = NETWORK_TABLE.getEntry(key);
-        if(when.test(entry)) return then.apply(entry, arg);
-        else return orElse.apply(entry, arg);
-    }
-
-    private static void nothing(NetworkTableEntry entry) {}
-    private static <T> T nothing(NetworkTableEntry entry, T ignored) {return null;}
-
-    private static void ifExists(String key, Consumer<NetworkTableEntry> run) {
-        ifOrElse(key, NetworkTableEntry::exists, run, NetworkUtilities::nothing);
-    }
-
-    private static <T> T ifExists(String key, BiFunction<NetworkTableEntry, T, T> get, T arg) {
-        return ifOrElse(key, NetworkTableEntry::exists, arg, get, NetworkUtilities::nothing);
+    private static void ifExists(String entryKey, Consumer<NetworkTableEntry> run) {
+        ifOrElse(entryKey, NetworkTableEntry::exists, run, NetworkUtilities::nothing);
     }
 }
